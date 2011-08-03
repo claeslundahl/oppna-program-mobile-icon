67cd6849c6d3d7f02fde14eee9cb9acb5b5656f6
wmdaros <wmdaros@465a72d9-5038-cd30-490a-49d6e36aa146>
2011-08-03 09:28:39 +0000

    - GET/POST

--- core-bc/modules/intsvc/src/main/java/se/vgregion/mobileicon/service/TestCounter.java
+++ core-bc/modules/intsvc/src/main/java/se/vgregion/mobileicon/service/TestCounter.java
@@ -18,8 +18,21 @@ public class TestCounter {
     @POST
     @Produces("application/json")
     public String getRandom() {
+        String msg = get();
+        System.out.println("POST: "+msg);
+        return msg;
+    }
+
+    @GET
+    @Produces("application/json")
+    public String random() {
+        String msg = get();
+        System.out.println("GET: "+msg);
+        return msg;
+    }
+
+    private String get() {
         Random random = new Random();
         return random.nextInt(100) + "";
     }
-
 }
