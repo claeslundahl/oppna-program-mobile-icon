63a4e9d2033bfb279349f27c93f1cef338ad65e0
wmdaros@gmail.com <wmdaros@gmail.com@465a72d9-5038-cd30-490a-49d6e36aa146>
2011-09-29 16:51:13 +0000

    - Add/Edit/Delete of Company Expando values - no validation/no styling

--- trunk/core-bc/composites/svc/src/main/java/se/vgregion/mobile/CompanyExpandoService.java
+++ trunk/core-bc/composites/svc/src/main/java/se/vgregion/mobile/CompanyExpandoService.java
@@ -1,80 +1,21 @@
 package se.vgregion.mobile;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.liferay.portlet.expando.model.ExpandoColumn;
 
-import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.exception.SystemException;
-import com.liferay.portal.model.Company;
-import com.liferay.portlet.expando.model.ExpandoColumnConstants;
-import com.liferay.portlet.expando.model.ExpandoTableConstants;
-import com.liferay.portlet.expando.service.ExpandoColumnLocalService;
-import com.liferay.portlet.expando.service.ExpandoTableLocalService;
-import com.liferay.portlet.expando.service.ExpandoValueLocalService;
+import java.util.List;
 
-public class CompanyExpandoService {
+/**
+ * Created by IntelliJ IDEA.
+ * User: david
+ * Date: 29/9-11
+ * Time: 15:55
+ */
+public interface CompanyExpandoService {
+    void setSetting(String columnName, String value, long companyId);
 
-	@Autowired
-	private ExpandoColumnLocalService expandoColumnService;
+    String getSetting(String columnName, long companyId);
 
-	@Autowired
-	private ExpandoTableLocalService expandoTableService;
+    List<ExpandoColumn> getAllKeys(long companyId);
 
-	@Autowired
-	private ExpandoValueLocalService expandoValueService;
-
-	public void setSetting(String columnName, String value, String defaultValue, long companyId) {
-		try {
-			expandoValueService.addValue(companyId, COMPANY_CLASSNAME, ExpandoTableConstants.DEFAULT_TABLE_NAME,
-			        columnName, companyId, value);
-		} catch (PortalException e) {
-			// If table don't exists we try to create it.
-			if (e instanceof com.liferay.portlet.expando.NoSuchTableException) {
-				createExpandoTabel(columnName, value, defaultValue, companyId);
-			} else if (e instanceof com.liferay.portlet.expando.NoSuchColumnException) {
-				// If column don't exists we try to create it.
-				createExpandoColumn(columnName, value, defaultValue, companyId);
-			} else {
-				throw new RuntimeException(e);
-			}
-		} catch (SystemException e) {
-			throw new RuntimeException(e);
-		}
-	}
-
-	public String getSetting(String columnName, long companyId) {
-
-		String value = "";
-		try {
-			value = expandoValueService.getData(companyId, COMPANY_CLASSNAME,
-			        ExpandoTableConstants.DEFAULT_TABLE_NAME, columnName, companyId, "");
-
-		} catch (PortalException e) {
-			throw new RuntimeException(e);
-		} catch (SystemException e) {
-			throw new RuntimeException(e);
-		}
-		return value;
-	}
-
-	private void createExpandoColumn(String columnName, String value, String defaultValue, long companyId) {
-		try {
-			long tableId = expandoTableService.getDefaultTable(companyId, COMPANY_CLASSNAME).getTableId();
-			expandoColumnService.addColumn(tableId, columnName, ExpandoColumnConstants.STRING, defaultValue);
-			setSetting(columnName, value, defaultValue, companyId);
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
-
-	private void createExpandoTabel(String columnName, String value, String defaultValue, long companyId) {
-		try {
-			expandoTableService.addDefaultTable(companyId, COMPANY_CLASSNAME);
-			setSetting(columnName, value, defaultValue, companyId);
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
-
-	private final static String COMPANY_CLASSNAME = Company.class.getName();
-
+    void delete(long companyId, String columnName);
 }
